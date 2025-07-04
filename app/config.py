2b7a72b25d519fee6bf3c7037f5debd31c3bad05
sinha6318 <sinha6318@gmail.com>
2025-07-04 11:15:34 +0530

    Milestone-VP DB-Relationship

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your_secret_key'  # You can make this anything
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///parking_app.db'  # SQLite DB file
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Just turns off a warning
+    SECRET_KEY = 'your_secret_key'  
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///parking_app.db'  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
