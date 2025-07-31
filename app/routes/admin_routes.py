03d57d5274578228550bd60eeea3f05030427b28
sinha6318 <sinha6318@gmail.com>
2025-07-31 12:55:17 +0530

    yaml

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -123,18 +123,3 @@ def view_users():
 def view_all_reservations():
     reservations = Reservation.query.order_by(Reservation.start_time.desc()).all()
     return render_template('admi_view_reservation.html', reservations=reservations)
-
-@admin_bp.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('details')
-    filter_by = request.args.get('type')
-
-    results = []
-    if filter_by == 'user':
-        results = User.query.filter(User.username.ilike(f"%{query}%")).all()
-    elif filter_by == 'spot':
-        results = Parkingspot.query.filter(Parkingspot.id == int(query)).all()
-    elif filter_by == 'lot':
-        results = Parkinglot.query.filter(Parkinglot.location_name.ilike(f"%{query}%")).all()
-
-    return render_template('admin_search.html', results=results, filter_by=filter_by, query=query)
